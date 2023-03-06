f4590b9bf3b3ddbd46ff1fe994072f4e39288088
Rakib-Ahmed01 <rakibahmed34360@gmail.com>
2023-03-06 17:22:12 +0600

    before mantine core

--- mantine/mantine/src/App.jsx
+++ mantine/mantine/src/App.jsx
@@ -1,55 +1,233 @@
-import { Button, MantineProvider, Text } from '@mantine/core';
-import { createStyles } from '@mantine/styles';
+import {
+  ActionIcon,
+  Box,
+  Button,
+  MantineProvider,
+  Paper,
+  Slider,
+  Text,
+  Tooltip,
+} from '@mantine/core';
+import {
+  useColorScheme,
+  useDisclosure,
+  useHotkeys,
+  useLocalStorage,
+  useShallowEffect,
+} from '@mantine/hooks';
+import {
+  ColorSchemeProvider,
+  createStyles,
+  rem,
+  useMantineColorScheme,
+} from '@mantine/styles';
+import { IconBrandTwitter, IconMoon, IconSun } from '@tabler/icons-react';
 import { useState } from 'react';
+import { HeaderMenuColored } from './components/Header';
 
-const useStyles = createStyles((theme) => ({
-  button: {
-    backgroundColor:
-      theme.colorScheme === 'dark' ? theme.white : theme.colors.cyan[5],
-    // fontWeight: 'normal',
-    // padding: theme.spacing.md,
-    // display: 'block',
-  },
-}));
+const useStyles = createStyles((theme, other) => {
+  // console.log(other);
+  // console.log(theme.fn.fontStyles());
+
+  // console.log(theme.colorScheme);
+  return {
+    // button: {
+    //   backgroundColor:
+    //     theme.colorScheme === 'dark' ? theme.white : theme.colors.cyan[5],
+    //   fontWeight: 'normal',
+    //   // padding: theme.spacing.md,
+    //   // display: 'block',
+    //   '&.has-shadow': {
+    //     boxShadow: theme.shadows.md,
+    //   },
+    // },
+    myCustomText: {
+      ...theme.fn.fontStyles(),
+    },
+  };
+});
 
 function App() {
-  const [theme, setTheme] = useState('light');
+  // const [theme, setTheme] = useState('light');
+  const [opened, handlers] = useDisclosure();
+  const preferredColorScheme = useColorScheme();
+  const [colorScheme, setColorScheme] = useLocalStorage({
+    key: 'mantine-color-scheme',
+    defaultValue: preferredColorScheme,
+    getInitialValueInEffect: true,
+  });
 
-  const { classes } = useStyles();
+  const obj = {};
+
+  // useEffect(() => {
+  //   console.log('inside useEffect');
+  // }, [obj]);
+
+  useShallowEffect(() => {
+    console.log('inside useShallowEffect');
+  }, [obj]);
+
+  useHotkeys([['alt+J', () => toggleColorScheme()]]);
+
+  const toggleColorScheme = (value) => {
+    setColorScheme(value || (colorScheme === 'light' ? 'dark' : 'light'));
+  };
+
+  const [mark, setmark] = useState(0);
+
+  const { classes } = useStyles({ colorScheme, opened, handlers });
 
   return (
-    <MantineProvider
-      withGlobalStyles
-      withNormalizeCSS
-      theme={{
-        fontFamily: 'Karla',
-        colorScheme: theme,
-        shadows: {
-          xs: '0.0625rem 0.0625rem 0.0625rem rgba(0, 0, 0, 0.3)',
-          sm: '0.0625rem 0.0625rem 0.0625rem rgba(0, 0, 0, 0.4)',
-          md: '0.1875rem 0.1875rem 0.0625rem rgba(0, 0, 0, 0.4)',
-          lg: '0.1875rem 0.1875rem 0.0625rem 0.5rem rgba(0, 0, 0, 0.4)',
-          xl: '0.1875rem 0.1875rem 0.0625rem 1rem rgba(0, 0, 0, 0.4)',
-        },
-      }}
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}
     >
-      <Text>Bismillah...</Text>
-      <Button
-        className={classes.button}
-        onClick={() =>
-          setTheme((theme) => (theme === 'dark' ? 'ligth' : 'dark'))
-        }
-        size="sm"
-        radius="lg"
-        color="cyan"
-        styles={(theme) => ({
-          fontWeight: 'normal',
-        })}
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        disableAnimations
+        theme={{
+          fontFamily: 'Karla',
+          colorScheme,
+          primaryColor: 'blue',
+          colors: {
+            dark: [
+              '#d5d7e0',
+              '#acaebf',
+              '#8c8fa3',
+              '#666980',
+              '#4d4f66',
+              '#34354a',
+              '#2b2c3d',
+              '#1d1e30',
+              '#0c0d21',
+              '#01010a',
+            ],
+          },
+        }}
       >
-        Change Theme
-      </Button>
-    </MantineProvider>
+        <HeaderMenuColored />
+        <Button
+          // className="bg-teal-500 shadow-2xl"
+          onClick={() => toggleColorScheme()}
+          size="sm"
+          radius="lg"
+          // styles={(theme) => ({
+          //   fontWeight: theme.spacing.md,
+          // })}
+          sx={(theme) => ({
+            boxShadow: theme.shadows.md,
+            '&:hover': {
+              backgroundColor: 'red',
+            },
+            // fontWeight: 'normal',
+            margin: '0 auto',
+          })}
+          fw="normal"
+          display={'inline-block'}
+          mx="auto"
+        >
+          <Text color="white">Change Theme</Text>
+        </Button>
+        <Paper
+          m={16}
+          p={16}
+          // shadow="sm"
+          className="shadow-md"
+          sx={(theme) => ({
+            '&:hover': {
+              backgroundColor: theme.colors.red[5],
+              color: 'white',
+            },
+            [theme.fn.smallerThan('sm')]: {
+              '&:hover': {
+                backgroundColor: theme.colors.green[5],
+                color: 'white',
+              },
+            },
+          })}
+        >
+          Alhamdulillah
+        </Paper>
+        <Button
+          // className="bg-indigo-500 text-white font-normal"
+          sx={(theme) => ({
+            fontWeight: 'lighter',
+          })}
+        >
+          This Button Styled With Tailwind
+        </Button>
+        <Box
+          bg={{ base: 'blue', sm: 'cyan', md: 'indigo' }}
+          p="lg"
+          // m="xs"
+          // color="#fff"
+          w={'98%'}
+          mt="xs"
+          mx="auto"
+          sx={(theme) => ({
+            borderRadius: rem(2),
+            color: 'white',
+          })}
+        >
+          This is a box
+          <Slider
+            color={'cyan'}
+            styles={{ thumb: { backgroundColor: 'blue' } }}
+            value={mark}
+            onChange={setmark}
+            marks={[
+              { value: 0 },
+              { value: 25 },
+              { value: 50 },
+              { value: 75 },
+              { value: 100 },
+            ]}
+          />
+        </Box>
+        <Button
+          leftIcon={<IconBrandTwitter />}
+          m="xs"
+          display={'inline-block'}
+          fw="normal"
+          // sx={(theme) => ({
+          //   padding: theme.spacing.xs,
+          // })}
+          styles={(theme) => ({
+            root: {
+              // paddingTop: rem(16),
+              // height: rem(42),
+              padding: `0 ${rem(30)}`,
+            },
+            leftIcon: {
+              color: 'yellow',
+            },
+          })}
+          h={50}
+        >
+          Follow on Twitter
+        </Button>
+        <Demo />
+      </MantineProvider>
+    </ColorSchemeProvider>
   );
 }
 
 export default App;
+
+function Demo() {
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+
+  const dark = colorScheme === 'dark';
+
+  return (
+    <Tooltip label="Toggle Theme">
+      <ActionIcon
+        color={dark ? 'yellow' : 'blue'}
+        onClick={() => toggleColorScheme()}
+      >
+        {dark ? <IconMoon /> : <IconSun />}
+      </ActionIcon>
+    </Tooltip>
+  );
+}
---
+++ mantine/mantine/src/components/Header.jsx
@@ -0,0 +1,224 @@
+import {
+  Burger,
+  Center,
+  Container,
+  createStyles,
+  Group,
+  Header,
+  Menu,
+  rem,
+} from '@mantine/core';
+import { MantineLogo } from '@mantine/ds';
+import { useClickOutside, useDisclosure } from '@mantine/hooks';
+import { IconChevronDown } from '@tabler/icons-react';
+
+const useStyles = createStyles((theme) => {
+  // console.log(
+  //   theme.fn.variant({
+  //     variant: 'filled',
+  //     color: theme.colors.cyan,
+  //   })
+  // );
+  return {
+    header: {
+      backgroundColor: theme.fn.variant({
+        variant: 'filled',
+        color: theme.primaryColor,
+      }).background,
+      borderBottom: 0,
+      position: 'relative',
+      transitionDuration: '2s',
+      transition: 'all',
+    },
+
+    inner: {
+      height: rem(56),
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+    },
+
+    links: {
+      [theme.fn.smallerThan('sm')]: {
+        display: 'none',
+      },
+    },
+
+    linksOnMobile: {
+      [theme.fn.largerThan('sm')]: {
+        display: 'none',
+      },
+      position: 'absolute',
+      top: '100%',
+      backgroundColor: theme.fn.variant({
+        variant: 'filled',
+        color: theme.colors.cyan,
+      }).background,
+      padding: theme.spacing.sm,
+      left: 0,
+      right: 0,
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'start',
+    },
+
+    burger: {
+      [theme.fn.largerThan('sm')]: {
+        display: 'none',
+      },
+    },
+
+    link: {
+      display: 'block',
+      lineHeight: 1,
+      padding: `${rem(8)} ${rem(12)}`,
+      borderRadius: theme.radius.sm,
+      textDecoration: 'none',
+      color: theme.white,
+      fontSize: theme.fontSizes.sm,
+      fontWeight: 500,
+
+      '&:hover': {
+        backgroundColor: theme.fn.lighten(
+          theme.fn.variant({ variant: 'filled', color: theme.primaryColor })
+            .background,
+          0.1
+        ),
+      },
+    },
+
+    linkLabel: {
+      marginRight: rem(5),
+    },
+  };
+});
+
+const links = [
+  {
+    link: '/about',
+    label: 'Features',
+  },
+  {
+    link: '#1',
+    label: 'Learn',
+    links: [
+      {
+        link: '/docs',
+        label: 'Documentation',
+      },
+      {
+        link: '/resources',
+        label: 'Resources',
+      },
+      {
+        link: '/community',
+        label: 'Community',
+      },
+      {
+        link: '/blog',
+        label: 'Blog',
+      },
+    ],
+  },
+  {
+    link: '/about',
+    label: 'About',
+  },
+  {
+    link: '/pricing',
+    label: 'Pricing',
+  },
+  {
+    link: '#2',
+    label: 'Support',
+    links: [
+      {
+        link: '/faq',
+        label: 'FAQ',
+      },
+      {
+        link: '/demo',
+        label: 'Book a demo',
+      },
+      {
+        link: '/forums',
+        label: 'Forums',
+      },
+    ],
+  },
+];
+
+export function HeaderMenuColored() {
+  const [opened, { toggle, close }] = useDisclosure(false);
+  const { classes } = useStyles();
+
+  const ref = useClickOutside(() => close());
+
+  const items = links.map((link) => {
+    const menuItems = link.links?.map((item) => (
+      <Menu.Item key={item.link}>{item.label}</Menu.Item>
+    ));
+
+    if (menuItems) {
+      return (
+        <Menu
+          key={link.label}
+          trigger="hover"
+          transitionProps={{
+            exitDuration: 0,
+            duration: 2,
+            transition: 2000,
+            timingFunction: 'ease',
+          }}
+          // withinPortal
+        >
+          <Menu.Target>
+            <a
+              href={link.link}
+              className={classes.link}
+              onClick={(event) => event.preventDefault()}
+            >
+              <Center>
+                <span className={classes.linkLabel}>{link.label}</span>
+                <IconChevronDown size="0.9rem" stroke={1.5} />
+              </Center>
+            </a>
+          </Menu.Target>
+          <Menu.Dropdown>{menuItems}</Menu.Dropdown>
+        </Menu>
+      );
+    }
+
+    return (
+      <a
+        key={link.label}
+        href={link.link}
+        className={classes.link}
+        onClick={(event) => event.preventDefault()}
+      >
+        {link.label}
+      </a>
+    );
+  });
+
+  return (
+    <Header ref={ref} height={56} className={classes.header} mb={50}>
+      <Container>
+        <div className={classes.inner}>
+          <MantineLogo size={28} inverted />
+          <Group spacing={5} className={classes.links}>
+            {items}
+          </Group>
+          {opened && <Group className={classes.linksOnMobile}>{items}</Group>}
+          <Burger
+            opened={opened}
+            onClick={toggle}
+            className={classes.burger}
+            size="sm"
+            color="#fff"
+          />
+        </div>
+      </Container>
+    </Header>
+  );
+}
