27aad70adc3f8a1d35a5595a98d58161ee620441
Rakib-Ahmed01 <rakibahmed34360@gmail.com>
2023-05-03 15:53:42 +0600

    structural typing and type predicate

--- typescript/books/learning-typescript/dist/index.js
+++ typescript/books/learning-typescript/dist/index.js
@@ -10,3 +10,20 @@ something.toUpperCase();
 something = 12345;
 // something.toUpperCase(); // Cannot do it
 exports.country = 'Bangladesh';
+const hasBoth = {
+    firstName: 'Rakib',
+    lastName: 'Ahmed',
+};
+const firstName = hasBoth;
+const lastName = hasBoth;
+console.log(firstName);
+let user1 = { name: 'John', age: 20, email: 'john@gmail.com' };
+function isUser(user) {
+    return user.email !== undefined && user.id !== undefined;
+}
+if (isUser(user1)) {
+    console.log('user1 is User');
+}
+else {
+    console.log('user1 is person');
+}
--- typescript/books/learning-typescript/src/index.ts
+++ typescript/books/learning-typescript/src/index.ts
@@ -1,7 +1,7 @@
 let something;
 //     ^?
 
-something = 'something';
+something = 'something' as const;
 // ^?
 
 something.toUpperCase();
@@ -11,3 +11,42 @@ something = 12345;
 // something.toUpperCase(); // Cannot do it
 
 export const country = 'Bangladesh';
+
+// const firstName: string = null;
+
+// Structural Typing
+type WithFirstName = { firstName: string };
+type WithLastName = { lastName: string };
+
+const hasBoth = {
+  firstName: 'Rakib',
+  lastName: 'Ahmed',
+};
+
+const firstName: WithFirstName = hasBoth;
+const lastName: WithLastName = hasBoth;
+
+console.log(firstName);
+
+type User = {
+  id: number;
+  name: string;
+  email: string;
+};
+
+type Person = {
+  name: string;
+  age: number;
+};
+
+let user1: User | Person = { name: 'John', age: 20, email: 'john@gmail.com' };
+
+function isUser(user: User | Person): user is User {
+  return (user as User).email !== undefined && (user as User).id !== undefined;
+}
+
+if (isUser(user1)) {
+  console.log('user1 is User');
+} else {
+  console.log('user1 is person');
+}
