c6eed86a883fd539aa0f5c90705b7a577baced36
Rakib-Ahmed01 <rakibahmed34360@gmail.com>
2023-05-04 14:58:52 +0600

    01-dr-on done

--- typescript/books/learning-typescript/learning-typescript-projects/projects/functions/secret-secrets/02-dr-on/index.ts
+++ typescript/books/learning-typescript/learning-typescript-projects/projects/functions/secret-secrets/02-dr-on/index.ts
@@ -1,2 +1,29 @@
 // Write your createAdvancedCipher function here! ✨
 // You'll need to export it so the tests can run it.
+
+type CipherFunction = (char: string) => string;
+
+function createAdvancedCipher(
+	onVowel: CipherFunction,
+	onConsonant: CipherFunction,
+	onPunctuation: CipherFunction
+) {
+	return (text: string): string => {
+		let result = "";
+
+		for (let char of text) {
+			const vowelRegex = /[aeiou]/i;
+			const consonantRegex = /[bcdfghjklmnpqrstvwxyz]/i;
+
+			if (vowelRegex.test(char)) {
+				result += onVowel(char);
+			} else if (consonantRegex.test(char)) {
+				result += onConsonant(char);
+			} else {
+				result += onPunctuation(char);
+			}
+		}
+
+		return result;
+	};
+}
