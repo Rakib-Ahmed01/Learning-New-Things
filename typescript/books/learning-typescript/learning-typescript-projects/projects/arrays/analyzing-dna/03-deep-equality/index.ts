a8c741fa7506bb4002b0c8347cce09a313ce34fc
Rakib-Ahmed01 <rakibahmed34360@gmail.com>
2023-05-04 23:38:06 +0600

    02-3-deep-equality done

--- typescript/books/learning-typescript/learning-typescript-projects/projects/arrays/analyzing-dna/03-deep-equality/index.ts
+++ typescript/books/learning-typescript/learning-typescript-projects/projects/arrays/analyzing-dna/03-deep-equality/index.ts
@@ -1,2 +1,19 @@
 // Write your deepEquality function here! ✨
 // You'll need to export it so the tests can run it.
+
+export function deepEquality(a: string | string[][], b: string | string[][]) {
+	if (a.length !== b.length) return false;
+
+	return JSON.stringify(a) === JSON.stringify(b);
+}
+
+[
+	[[], []],
+	[[["a"]], [[]]],
+	[[[]], [["a"]]],
+	[[["a"]], [["a"]]],
+	[[["a"]], [["c"]]],
+	[[["a", "c"]], [["a", "c"]]],
+].map((arrs) => {
+	console.log(deepEquality(arrs[0], arrs[1]));
+});
