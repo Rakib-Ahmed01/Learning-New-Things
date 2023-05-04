7d4a7e94d90f08d38c7fb7922f056d2a91147e34
Rakib-Ahmed01 <rakibahmed34360@gmail.com>
2023-05-05 00:14:47 +0600

    04-deep-differences done

--- typescript/books/learning-typescript/learning-typescript-projects/projects/arrays/analyzing-dna/04-deep-differences/index.ts
+++ typescript/books/learning-typescript/learning-typescript-projects/projects/arrays/analyzing-dna/04-deep-differences/index.ts
@@ -1,2 +1,37 @@
 // Write your deepDifferences function here! ✨
 // You'll need to export it so the tests can run it.
+
+export function deepDifferences(a: string[][], b: string[][]) {
+	if (a.length !== b.length) return [];
+
+	// let result: (string | undefined)[] | (string | undefined)[][];
+
+	const result = a.map((elem, i) => {
+		if (elem.length !== b[i].length) return undefined;
+
+		return elem.map((elem, j) => {
+			return elem === b[i][j] ? elem : undefined;
+		});
+	});
+
+	return result;
+}
+
+[
+	[[], []],
+	[[["a"]], [[]]],
+	[[[]], [["a"]]],
+	[[["a"]], [["a"]]],
+	[[["a"]], [["c"]]],
+	[[["a", "c"]], [["a", "c"]], [["a", "c"]]],
+	[
+		[["a"], ["c"]],
+		[["a"], ["g"]],
+	],
+	[
+		[["a"], ["c"]],
+		[["g"], ["c"]],
+	],
+].map((arrs) => {
+	console.log(deepDifferences(arrs[0], arrs[1]));
+});
