dfaf31dab20a6084d359fa836aebd09c2ba2acac
Rakib-Ahmed01 <rakibahmed34360@gmail.com>
2023-05-04 22:46:40 +0600

    01-shallow-equality done

--- typescript/books/learning-typescript/learning-typescript-projects/projects/arrays/analyzing-dna/01-shallow-equality/index.ts
+++ typescript/books/learning-typescript/learning-typescript-projects/projects/arrays/analyzing-dna/01-shallow-equality/index.ts
@@ -1,2 +1,8 @@
 // Write your shallowEquality function here! ✨
 // You'll need to export it so the tests can run it.
+
+export function shallowEquality(a: string[], b: string[]) {
+	return JSON.stringify(a) === JSON.stringify(b);
+}
+
+console.log(shallowEquality(["a", "c", "g", "t"], ["a", "c", "g", "t"]));
