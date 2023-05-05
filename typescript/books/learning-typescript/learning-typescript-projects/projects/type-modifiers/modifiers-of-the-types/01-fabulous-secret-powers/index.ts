a8d1863423dd015f6c88c0318d0499aac4933110
Rakib-Ahmed01 <rakibahmed34360@gmail.com>
2023-05-05 23:39:55 +0600

    01-fabulous-secret-powers done

--- typescript/books/learning-typescript/learning-typescript-projects/projects/type-modifiers/modifiers-of-the-types/01-fabulous-secret-powers/index.ts
+++ typescript/books/learning-typescript/learning-typescript-projects/projects/type-modifiers/modifiers-of-the-types/01-fabulous-secret-powers/index.ts
@@ -1,4 +1,20 @@
-import { characters } from "./characters";
-
 // Write your announceCharacter function here! ✨
 // You'll need to export it so the tests can run it.
+
+import { characters } from "./characters";
+
+interface Character {
+	name: string;
+	powers: string[];
+	side: "good" | "evil";
+}
+
+export function announceCharacter(stringifiedCharacter: string) {
+	const character = JSON.parse(stringifiedCharacter) as Character;
+
+	console.log(`I am ${character.name}.`);
+	console.log(`My powers are: ${character.powers.join(", ")}.`);
+	console.log(`I am ${character.side}.`);
+}
+
+announceCharacter(characters[2]);
