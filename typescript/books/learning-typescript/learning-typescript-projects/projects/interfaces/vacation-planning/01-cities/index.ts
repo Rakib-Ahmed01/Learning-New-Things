a8b85077562a843c00575bc3719451ecfa9a6f35
Rakib-Ahmed01 <rakibahmed34360@gmail.com>
2023-05-05 15:35:26 +0600

    01-cities done

--- typescript/books/learning-typescript/learning-typescript-projects/projects/interfaces/vacation-planning/01-cities/index.ts
+++ typescript/books/learning-typescript/learning-typescript-projects/projects/interfaces/vacation-planning/01-cities/index.ts
@@ -1,2 +1,21 @@
 // Write your describeCity function here! ✨
 // You'll need to export it so the tests can run it.
+
+interface Coordinates {
+	north: [number, number, number];
+	west: [number, number, number];
+}
+
+interface City {
+	coordinates: Coordinates;
+	name: string;
+	catchphrase?: string;
+}
+
+export default function describeCity(city: City) {
+	const latitude = `${city.coordinates.north[0]}°${city.coordinates.north[1]}'${city.coordinates.north[2]}"N`;
+	const longitude = `${city.coordinates.west[0]}°${city.coordinates.west[1]}'${city.coordinates.west[2]}"W`;
+	const formattedCoordinates = `${latitude} ${longitude}`;
+
+	return `${city.name}\n* "${city.catchphrase}"\n* Located at ${formattedCoordinates}`;
+}
