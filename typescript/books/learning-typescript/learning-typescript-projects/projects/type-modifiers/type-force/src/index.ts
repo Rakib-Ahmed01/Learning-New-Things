7240f28256edecaa71efd869101aebdd17ddd9d0
Rakib-Ahmed01 <rakibahmed34360@gmail.com>
2023-05-06 01:39:30 +0600

    type-force done

--- typescript/books/learning-typescript/learning-typescript-projects/projects/type-modifiers/type-force/src/index.ts
+++ typescript/books/learning-typescript/learning-typescript-projects/projects/type-modifiers/type-force/src/index.ts
@@ -1,32 +1,41 @@
 // Write your duel function and types below! ✨
 // You'll need to export duel so the tests can run it.
 
+interface Character {
+	flying: boolean;
+	name: string;
+	toughness: number;
+	power: number;
+}
+
 const mutationsLibrary = {
-	energy: (hero) => {
+	energy: (hero: Character) => {
 		hero.power *= 1.25;
 		hero.flying = true;
 	},
-	healing: (hero) => {
+	healing: (hero: Character) => {
 		hero.toughness *= 2;
 	},
-	luck: (hero) => {
+	luck: (hero: Character) => {
 		hero.power *= 1.25;
 		hero.toughness *= 1.25;
 	},
-	flight: (hero) => {
+	flight: (hero: Character) => {
 		hero.flying = true;
 	},
-	strength: (hero) => {
+	strength: (hero: Character) => {
 		hero.power *= 2;
 	},
-	wings: (hero) => {
+	wings: (hero: Character) => {
 		hero.flying = true;
 		hero.toughness *= 0.9;
 	},
 };
 
-function createCharacter(name, mutations) {
-	const character = {
+type Mutation = keyof typeof mutationsLibrary;
+
+function createCharacter(name: string, mutations: Mutation[]) {
+	const character: Character = {
 		flying: false,
 		name,
 		power: 1,
@@ -39,3 +48,30 @@ function createCharacter(name, mutations) {
 
 	return character;
 }
+
+interface Fighter {
+	name: string;
+	mutations: Mutation[];
+}
+
+export function duel(good: Fighter, bad: Fighter) {
+	const hero = createCharacter(good.name, good.mutations);
+	const villain = createCharacter(bad.name, bad.mutations);
+
+	return hero.power / villain.toughness >= villain.power / hero.toughness
+		? (["hero", hero] as const)
+		: (["villain", villain] as const);
+}
+
+console.log(
+	duel(
+		{
+			name: "Rakib Ahmed",
+			mutations: ["energy", "wings", "strength", "luck", "flight", "healing"],
+		},
+		{
+			name: "Shaitan",
+			mutations: ["energy", "flight", "wings"],
+		}
+	)
+);
