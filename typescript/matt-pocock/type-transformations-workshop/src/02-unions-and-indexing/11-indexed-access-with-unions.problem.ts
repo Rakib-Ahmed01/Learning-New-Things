211f61dabd1130930202c53047136e7247edf32b
Rakib-Ahmed01 <rakibahmed34360@gmail.com>
2023-05-08 02:47:39 +0600

    11 done

--- typescript/matt-pocock/type-transformations-workshop/src/02-unions-and-indexing/11-indexed-access-with-unions.problem.ts
+++ typescript/matt-pocock/type-transformations-workshop/src/02-unions-and-indexing/11-indexed-access-with-unions.problem.ts
@@ -1,21 +1,27 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils';
 
 export const programModeEnumMap = {
-  GROUP: "group",
-  ANNOUNCEMENT: "announcement",
-  ONE_ON_ONE: "1on1",
-  SELF_DIRECTED: "selfDirected",
-  PLANNED_ONE_ON_ONE: "planned1on1",
-  PLANNED_SELF_DIRECTED: "plannedSelfDirected",
+  GROUP: 'group',
+  ANNOUNCEMENT: 'announcement',
+  ONE_ON_ONE: '1on1',
+  SELF_DIRECTED: 'selfDirected',
+  PLANNED_ONE_ON_ONE: 'planned1on1',
+  PLANNED_SELF_DIRECTED: 'plannedSelfDirected',
 } as const;
 
-export type IndividualProgram = unknown;
+type ProgramModeEnumMap = typeof programModeEnumMap;
+type ProgramModeEnumMapKeys = keyof typeof programModeEnumMap;
+
+export type IndividualProgram = Exclude<
+  ProgramModeEnumMap[ProgramModeEnumMapKeys],
+  'group' | 'announcement'
+>;
 
 type tests = [
   Expect<
     Equal<
       IndividualProgram,
-      "1on1" | "selfDirected" | "planned1on1" | "plannedSelfDirected"
+      '1on1' | 'selfDirected' | 'planned1on1' | 'plannedSelfDirected'
     >
-  >,
+  >
 ];
