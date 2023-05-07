d4ac3a5dc1fe22c185db28074e474e226b576698
Rakib-Ahmed01 <rakibahmed34360@gmail.com>
2023-05-08 03:34:46 +0600

    28 done

--- typescript/matt-pocock/type-transformations-workshop/src/03.5-type-helpers-pattern/20.7-non-empty-array.problem.ts
+++ typescript/matt-pocock/type-transformations-workshop/src/03.5-type-helpers-pattern/20.7-non-empty-array.problem.ts
@@ -1,14 +1,14 @@
 type NonEmptyArray<T> = [T, ...T[]];
 
 export const makeEnum = <T>(values: NonEmptyArray<T>) => {
-  return [...values] as const;
+  return [...values];
 };
 
 makeEnum(['a']);
 makeEnum(['a', 'b', 'c']);
 const nums = makeEnum([1, 2, 3, 4]);
 
-nums;
+nums.push('1');
 
 // @ts-expect-error
 makeEnum([]);
