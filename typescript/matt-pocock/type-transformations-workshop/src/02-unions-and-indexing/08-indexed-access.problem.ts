4d10179e97133d54701dfe7b512c0ab879d3d671
Rakib-Ahmed01 <rakibahmed34360@gmail.com>
2023-05-08 02:32:18 +0600

    08 done

--- typescript/matt-pocock/type-transformations-workshop/src/02-unions-and-indexing/08-indexed-access.problem.ts
+++ typescript/matt-pocock/type-transformations-workshop/src/02-unions-and-indexing/08-indexed-access.problem.ts
@@ -1,23 +1,23 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils';
 
 export const fakeDataDefaults = {
-  String: "Default string",
+  String: 'Default string',
   Int: 1,
   Float: 1.14,
   Boolean: true,
-  ID: "id",
+  ID: 'id',
 };
 
-export type StringType = unknown;
-export type IntType = unknown;
-export type FloatType = unknown;
-export type BooleanType = unknown;
-export type IDType = unknown;
+export type StringType = (typeof fakeDataDefaults)['String'];
+export type IntType = (typeof fakeDataDefaults)['Int'];
+export type FloatType = (typeof fakeDataDefaults)['Float'];
+export type BooleanType = (typeof fakeDataDefaults)['Boolean'];
+export type IDType = (typeof fakeDataDefaults)['ID'];
 
 type tests = [
   Expect<Equal<StringType, string>>,
   Expect<Equal<IntType, number>>,
   Expect<Equal<FloatType, number>>,
   Expect<Equal<BooleanType, boolean>>,
-  Expect<Equal<IDType, string>>,
+  Expect<Equal<IDType, string>>
 ];
