86f98c78be7fe18d0be8b6b12f1b0bfb337d5131
RakibAhmed01 <rakibahmed34360@gmail.com>
2023-12-06 17:52:14 +0600

    expression slot

--- advanced-react/josh/src/main.jsx
+++ advanced-react/josh/src/main.jsx
@@ -1,25 +1,36 @@
-import { createElement } from 'react';
 import { createRoot } from 'react-dom/client';
 
 function render(reactElement, containerDOMElement) {
-  createRoot(containerDOMElement).render(
-    createElement(
-      reactElement.type,
-      { ...reactElement.props },
-      reactElement.children
-    )
-  );
+  // createRoot(containerDOMElement).render(
+  //   createElement(
+  //     reactElement.type,
+  //     { ...reactElement.props },
+  //     reactElement.children
+  //   )
+  // );
+  createRoot(containerDOMElement).render(reactElement);
 }
 
-const items = ['mango', 'apple', 'carrot'];
+const items = ['mango', 'pineapple', 'jackfruit'];
 
-const reactElement = {
-  type: 'div',
-  props: {
-    className: 'elem',
-  },
-  children: `Itmes left to purchase: ${items.length}`,
-};
+// const reactElement = {
+//   type: 'div',
+//   props: {
+//     className: 'elem',
+//   },
+//   children: `Itmes left to purchase: ${items.length}`,
+// };
+
+// const reactElement = createElement(
+//   'div',
+//   null,
+//   'Itmes left to purchase: ',
+//   items.length
+// );
+
+const reactElement = (
+  <div>Items left to purchase: {items.length} items left</div>
+);
 
 const containerDOMElement = document.getElementById('root');
 
